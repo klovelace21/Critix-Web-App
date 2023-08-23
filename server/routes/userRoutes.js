c63fa8c596356f63af5d9b20f0b6c44089d17008
Kenneth <kennethlovelace21@gmail.com>
2023-08-23 12:46:07 -0400

    created controllers and routes folders

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
+
+router.route('/')
+    .get()
+
+module.exports = router
