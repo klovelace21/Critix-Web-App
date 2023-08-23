91cb0c3522f75a92dbf837e72f34deb9fc143e55
Kenneth <kennethlovelace21@gmail.com>
2023-08-23 13:39:08 -0400

    logger middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3500
-
 const userRoute = require('./routes/userRoutes')
+const logger = require('./middleware/logger')
 
+app.use(logger)
 app.use('/user', userRoute)
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
