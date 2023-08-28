754bc99e5471fbc75bea345a64729a346a0a014f
Kenneth <kennethlovelace21@gmail.com>
2023-08-28 13:17:29 -0400

    react router practice, added TMDB logo

---
+++ client/src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, reprehenderit non! Incidunt facere assumenda voluptas! Facilis, ipsum illum ut inventore mollitia, fugit aut molestias officiis reprehenderit aspernatur quo quia praesentium?</p>
+    </div>
+  )
+}
+
+export default About
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse optio vero, porro dicta sed blanditiis totam fugit velit aspernatur repudiandae praesentium mollitia, cum dolor sunt quidem vel tempora ullam sapiente.</p>
+    </div>
+  )
+}
+
+export default Home
