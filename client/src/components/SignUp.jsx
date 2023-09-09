b8fc36a600c316e78331260384b9c99b27e7d296
Kenneth <kennethlovelace21@gmail.com>
2023-09-09 14:33:12 -0400

    refactored to remove favoriteGenre

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -3,48 +3,15 @@ import { createUser } from '../services/users'
 import { useEffect, useState, useRef } from 'react'
 import { faCheck, faTimes, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Select from 'react-select'
 import PropTypes from 'prop-types'
-import explosion from '../assets/icons/explosion.png'
-import hat from '../assets/icons/hat.png'
-import magicWand from '../assets/icons/magic_wand.png'
-import laughing from '../assets/icons/laughing.png'
-import handCuffs from '../assets/icons/handCuffs.png'
-import camera from '../assets/icons/camera.png'
-import theatre from '../assets/icons/theatre.png'
-import dragon from '../assets/icons/dragon.png'
-import hourGlass from '../assets/icons/hourglass.png'
-import ghost from '../assets/icons/ghost.png'
-import magnifyingGlass from '../assets/icons/magnifyingGlass.png'
-import heart from '../assets/icons/heart.png'
-import alien from '../assets/icons/alien.png'
-import knife from '../assets/icons/knife.png'
-import tank from '../assets/icons/tank.png'
-import cactus from '../assets/icons/cactus.png'
+
 
 
 const SignUp = ({ toggleAccount }) => {
   const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
   const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-  const genres = [
-    { value: "action", label: <div><img src={explosion} height="15px" width="15px"/>Action</div> },
-    { value: 'adventure', label: <div><img src={hat} height="15px" width="15px"/>Adventure</div> },
-    { value: 'animation', label: <div><img src={magicWand} height="15px" width="15px"/>Animation</div> },
-    { value: 'comedy', label: <div><img src={laughing} height="15px" width="15px"/>Comedy</div> },
-    { value: 'crime', label: <div><img src={handCuffs} height="15px" width="15px"/>Crime</div> },
-    { value: 'documentary', label: <div><img src={camera} height="15px" width="15px"/>Documentary</div> },
-    { value: 'drama', label: <div><img src={theatre} height="15px" width="15px"/>Drama</div> },
-    { value: 'fantasy', label: <div><img src={dragon} height="15px" width="15px"/>Fantasy</div> },
-    { value: 'history', label: <div><img src={hourGlass} height="15px" width="15px"/>History</div> },
-    { value: 'horror', label: <div><img src={ghost} height="15px" width="15px"/>Horror</div> },
-    { value: 'mystery', label: <div><img src={magnifyingGlass} height="15px" width="15px"/>Mystery</div> },
-    { value: 'romance', label: <div><img src={heart} height="15px" width="15px"/>Romance</div> },
-    { value: 'science fiction', label: <div><img src={alien} height="15px" width="15px"/>Science Fiction</div> },
-    { value: 'thriller', label: <div><img src={knife} height="15px" width="15px"/>Thriller</div> },
-    { value: 'war', label: <div><img src={tank} height="15px" width="15px"/>War</div> },
-    { value: 'western', label: <div><img src={cactus} height="15px" width="15px"/>Western</div> }
-  ]
+  
 
   const userRef = useRef()
   const errRef = useRef()
@@ -89,7 +56,7 @@ const SignUp = ({ toggleAccount }) => {
     setErrMsg('')
   }, [newUsername, newPassword, confirmPassword])
 
-  const [selectedOption, selectSelectedOption] = useState('')
+
 
   const addUser = async (e) => {
     e.preventDefault()
@@ -98,7 +65,6 @@ const SignUp = ({ toggleAccount }) => {
     await createUser({ 
       username: newUsername, 
       password: newPassword, 
-      favoriteGenre: selectedOption.value
     })
     setSuccess(true)
     } catch (err) {
@@ -117,7 +83,6 @@ const SignUp = ({ toggleAccount }) => {
     setUsername('')
     setPassword('')
     setConfirmPassword('')
-    selectSelectedOption('')
   }
   
   return (
@@ -206,8 +171,6 @@ const SignUp = ({ toggleAccount }) => {
         <FontAwesomeIcon icon={faInfoCircle} className='icon' />
         Must match the first password input field.
       </p>
-      <label>Select your favorite genre</label>
-      <Select className="genres" options={genres} onChange={(choice) => selectSelectedOption(choice)}/>
       <span className='error' ref={errRef}>{errorMsg}</span>
       <button disabled={!validName || !validPassword || !confirmMatch ? true : false}>Create Account</button>
      
